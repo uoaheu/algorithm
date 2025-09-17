467d74beec5f182bbc81d508d2b628c97eeb3e83
nayeon <96762267+uoaheu@users.noreply.github.com>
2025-09-17 19:12:31 +0900

    feat(boj/java): Silver3 정수_a를_k로_만들기 (250917)

---
+++ boj/Java/Silver/250917_25418_정수a를k로만들기.java
@@ -0,0 +1,36 @@
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int A = sc.nextInt();
+        int K = sc.nextInt();
+        Queue<int[]> q = new LinkedList<>();
+        boolean[] visited = new boolean[K + 1];
+        q.offer(new int[]{A, 0});
+        visited[A] = true;
+
+        while (!q.isEmpty()) {
+            int[] current = q.poll();
+            int num = current[0];
+            int count = current[1];
+
+            if (num == K) {
+                System.out.println(count);
+                break;
+            }
+
+            if (num + 1 <= K && !visited[num + 1]) {
+                visited[num + 1] = true;
+                q.offer(new int[]{num + 1, count + 1});
+            }
+
+            if (num * 2 <= K && !visited[num * 2]) {
+                visited[num * 2] = true;
+                q.offer(new int[]{num * 2, count + 1});
+            }
+        }
+    }
+}
